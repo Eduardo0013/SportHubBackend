d5f9e94b49443d4c3f13626cec8ac74433df994d
Eduardo0013 <thewarrior20013@gmail.com>
2024-10-07 00:48:41 -0600

    feat(auth): se completo la acción login

--- modules/auth/services/AuthService.ts
+++ modules/auth/services/AuthService.ts
@@ -1,10 +1,19 @@
-import { v4 } from 'uuid'
 import prisma from "../../../config/prisma"
 import User from "../../core/domain/models/Usuario"
 
-export const authenticate = (user : unknown) => {
-    /*const userModel : User = {
-        id : v4(),
-        nombre : user.name,
-    }*/
+/**
+ * 
+ * @param email 
+ * @param password 
+ * @returns
+ */
+export const authenticate = async (email : String, password : String) => {
+    const user : User = await prisma.user.findFirst({
+        where : {
+            password,
+            email
+        }
+    })
+
+    return user !== null;
 }
