dbada7fdb42a551cbe56efadd52f4ac9588b2960
Eduardo0013 <thewarrior20013@gmail.com>
2024-10-07 00:13:18 -0600

    refactor(core): se refactorizo la estructura de carpetas

---
+++ modules/auth/services/AuthService.ts
@@ -0,0 +1,10 @@
+import { v4 } from 'uuid'
+import prisma from "../../../config/prisma"
+import User from "../../core/domain/models/Usuario"
+
+export const authenticate = (user : unknown) => {
+    /*const userModel : User = {
+        id : v4(),
+        nombre : user.name,
+    }*/
+}
